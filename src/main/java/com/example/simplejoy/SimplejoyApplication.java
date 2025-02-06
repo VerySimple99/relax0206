5b2e6ea02347a47b0656d4fd42afbd312199fc16
VerySimple99 <lyricrain.seo@gmail.com>
2025-02-06 13:28:07 +0900

    joy simple

---
+++ src/main/java/com/example/simplejoy/SimplejoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplejoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplejoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplejoyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplejoy/SimplejoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplejoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplejoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
